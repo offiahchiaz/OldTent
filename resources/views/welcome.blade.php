399059a344ecbc2bdeb36a2239ed36e518f918ea
offiahchiaz <offiahchiaz@gmail.com>
2021-02-26 19:10:08 +0100

    Added navbar styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,47 +42,14 @@
             <div id="content">
 
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                    <div class="container" style="padding-left: 200px;">
+
+
+                {{-- navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow --}}
+                <nav class="navbar navbar-expand-lg navbar-light bg-white mb-4 static-top shadow" style="padding-bottom: 15px; margin-bottom: 20px;">
+                    {{-- <a class="navbar-brand" href="#">Tent</a> --}}
+                    <div style="padding-left: 100px;">
                         <a class="navbar-brand" href="/"><h4>Tent</h4></a>
                     </div>
-                   
-
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-                        <li class="nav-item no-arrow">
-                            <a class="nav-link" style="color: #413e5f;" href="{{ url('/apartments') }}">Apartments</a>
-                        </li>
-
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
-                        @if (Route::has('login'))
-                            @auth
-                                <li class="nav-item no-arrow">
-                                    <a class="nav-link" style="color: #413e5f;" href="{{ url('/home') }}">Home</a>
-                                </li>
-                                
-                                @else
-                                    <li class="nav-item no-arrow">
-                                        <a  class="nav-link" style="color: #413e5f;" href="{{ route('login') }}">Login</a>
-                                    </li>
-                                    @if (Route::has('register'))
-                                    <li class="nav-item no-arrow">
-                                        <a class="nav-link" style="color: #413e5f;" href="{{ route('register') }}">Register</a>
-                                    </li>
-                                    @endif
-                            @endauth
-                        @endif
-
-                    </ul>
-
-                </nav>
-
-
-
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Tent</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
@@ -112,25 +79,6 @@
                                     @endif
                             @endauth
                         @endif
-
-                        <div class="topbar-divider d-none d-sm-block"></div>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                          </a>
-                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                          </div>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                        </li>
                       </ul>
                     </div>
                   </nav>
